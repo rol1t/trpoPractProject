67a8267156a4ea713a8f410b8b5f4f17afa67228
alesya.benchuk <alesya.benchuk@mail.ru>
2019-04-17 16:21:57 +0300

    hiu

--- trpoMarkAnalizerProject/trpoMarkAnalizerProject/Form1.cs
+++ trpoMarkAnalizerProject/trpoMarkAnalizerProject/Form1.cs
@@ -16,5 +16,10 @@ namespace trpoMarkAnalizerProject
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Привет, Алексей!!");
+        }
     }
 }
